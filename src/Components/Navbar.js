29af8375bd27fc82ac04f4ecdcbfccc1d9d7e9b3
noumanali10 <alinaman1296@gmail.com>
2024-03-09 18:43:44 +0500

    push
    push

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-    <div className='container1 navbar h-[10vh] w-screen border border-purple-700 xsm:bg-white absolute '>
+    <div className='flex justify-around xsm:justify-between xsm:px-8 items-center navbar h-[10vh] w-screen border border-purple-700 bg-white absolute '>
         <div className="navleft  ">
             <div className="logo w-[97px]">
-                <img src="https://www.cubix.co/themes/responsiv-clean/assets/img/cubix-logo-dark.svg" alt="" />
+                <img src="https://www.cubix.co/themes/responsiv-clean/assets/img/cubix-logo-dark.svg" alt="logo" />
             </div>
         </div>
         <div className="navright flex">
-            <nav className='flex border border-green-900 min-w-24'>
+            <nav className='flex border border-green-900 min-w-24 grow'>
                 <ul className='flex w-full justify-between items-center border border-red-900'>
                    
                     <li><span className='border   bg-primary text-white h-11 w-11 text-sm  rounded-full flex items-center justify-center'><i class="fa-solid fa-phone"></i></span></li>
