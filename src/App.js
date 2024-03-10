fe5f8ba6ffbcd47007bbc41507ca948b02120aa1
noumanali10 <alinaman1296@gmail.com>
2024-03-10 12:08:46 +0500

    Revert "push"
    This reverts commit 613eb20981039dc6d67320fa4977cf3e8b3432e1.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 
 import './App.css';
-import CTA from './Components/CTA';
-import Footer from './Components/Footer';
-// import Navbar from './Components/Navbar';
-// import Hero from './Components/Hero'
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero'
 
 function App() {
   return (
     <>
-    {/* <Navbar/>
-    <Hero/> */}
-    <CTA/>
-    <Footer/>
+    <Navbar/>
+    <Hero/>
     </>
   );
 }
