1379707a0fc595863eb0d42dc5185cdc99019101
noumanali10 <alinaman1296@gmail.com>
2024-03-11 21:31:05 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 // Importing components. Uncomment the ones you need.
 // import ServicesDropDown from "./Components/ServicesDropDown";
-// import CTA from './Components/CTA';
-// import Footer from './Components/Footer';
+import CTA from './Components/CTA';
+import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Hero from "./Components/Hero";
 // If you decided not to use Hero, comment it out and use the other imports as needed.
@@ -14,8 +14,8 @@ function App() {
       <Navbar />
       <Hero />
       {/* Uncomment the components below as needed. */}
-      {/* <CTA />
-      <Footer /> */}
+      <CTA />
+      <Footer />
       {/* <ServicesDropDown /> */}
     </>
   );
