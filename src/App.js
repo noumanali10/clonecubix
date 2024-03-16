54d63c5d42da774a736f257f96d76ea3e778d552
noumanali10 <alinaman1296@gmail.com>
2024-03-16 14:42:38 +0500

    push

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./App.css";
 // Importing components. Uncomment the ones you need.
 // import ServicesDropDown from "./Components/ServicesDropDown";
-import CTA from "./Components/CTA";
-import Footer from "./Components/Footer";
+// import CTA from "./Components/CTA";
+// import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Section2 from "./Components/section2";
 // If you decided not to use Hero, comment it out and use the other imports as needed.
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
     <>
       <Navbar/>
         <Hero />
-        <CTA />
-        <Footer />
+        <Section2/>
+        {/* <CTA />
+        <Footer /> */}
      
     </>
   );
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,43 +1,56 @@
-import React from 'react'
-import { imgs } from '../Images/icons'
-import "../CSS/Hero.css"
+import React from "react";
+import { imgs } from "../Images/icons";
+import "../CSS/Hero.css";
 
 export default function Hero() {
   return (
     <>
-    <section className='hero text-white '>
-      <div className="mainhero flex justify-center items-center it w-screen   bg-hero-pattern bg-cover bg-center h-[85vh] ">
-        <div className='main w-11/12   h-full flex justify-center items-center "'>
-         <div className='h-[90%] w-full flex flex-col justify-evenly  '>
-          <div className=" heroText mt-6  h-1/2 flex flex-col items-center justify-evenly">
-            <h1 className='text-3xl font-bold '>Moderniing Mobile Platforms</h1>
-            <p className='font-normal text-base leading-relaxed self-center text-center  text-[rgba(255,255,255,.9)] w-full '>Custom mobile apps.User experiences & design.Engagement & Intelligence.Native & cross-platforms</p>
-            <div className='bg-primary px-4 py-1 rounded-sm w-40 h-10 text-center flex justify-center items-center'><button className="herobtn text-center bg-primary  text-sm font-bold  ">LETS' TALK</button></div>
-          </div> 
-          <div className="heroLower  flex flex-col justify-around bg-[#000000b0] rounded h-1/3">
-            <div className="top flex flex-col items-center h-2/5 justify-around">
-              <h4 className='font-medium text-xl'>Trusted by</h4>
-              <div className='topimgs flex w-full justify-around '>
-                <img className='topimg' src={imgs.leftimg1} alt="" />
-                <img className='topimg' src={imgs.leftimg2} alt="" />
-                <img className='topimg' src={imgs.leftimg3} alt="" />
-                <img className='topimg' src={imgs.leftimg4} alt="" />
+      <section className="hero text-white ">
+        <div className="mainhero flex justify-center items-center it w-screen   bg-hero-pattern bg-cover bg-center h-[85vh] ">
+          <div className='main w-11/12   h-full flex justify-center items-center "'>
+            <div className="h-[90%] w-full flex flex-col justify-evenly  ">
+              <div className=" heroText mt-6  h-1/2 flex flex-col items-center justify-evenly">
+                <h1 className="text-3xl font-bold ">
+                  Moderniing Mobile Platforms
+                </h1>
+                <p className="font-normal text-base leading-relaxed self-center text-center  text-[rgba(255,255,255,.9)] w-full ">
+                  Custom mobile apps.User experiences & design.Engagement &
+                  Intelligence.Native & cross-platforms
+                </p>
+                <div className="bg-primary px-4 py-1 rounded-sm w-40 h-10 text-center flex justify-center items-center">
+                  <button className="herobtn text-center bg-primary  text-sm font-bold  ">
+                    LETS' TALK
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="lower flex flex-col h-2/5 justify-around">
-              <h4 className='self-center font-medium text-xl'>Awards</h4>
-              <div className="lowerimgs flex w-full justify-around items-center">
-                <img src={imgs.rightimg1} alt="" className="lowerimg" />
-                <img src={imgs.rightimg2} alt="" className="lowerimg h-[29.52px]" />
-                <img src={imgs.rightimg3} alt="" className="lowerimg" />
-                <img src={imgs.rightimg4} alt="" className="lowerimg" />
+              <div className="heroLower  flex flex-col justify-around bg-[#000000b0] rounded h-1/3">
+                <div className="top flex flex-col items-center h-2/5 justify-around">
+                  <h4 className="font-medium text-xl">Trusted by</h4>
+                  <div className="topimgs flex w-full justify-around ">
+                    <img className="topimg" src={imgs.leftimg1} alt="" />
+                    <img className="topimg" src={imgs.leftimg2} alt="" />
+                    <img className="topimg" src={imgs.leftimg3} alt="" />
+                    <img className="topimg" src={imgs.leftimg4} alt="" />
+                  </div>
+                </div>
+                <div className="lower flex flex-col h-2/5 justify-around">
+                  <h4 className="self-center font-medium text-xl">Awards</h4>
+                  <div className="lowerimgs flex w-full justify-around items-center">
+                    <img src={imgs.rightimg1} alt="" className="lowerimg" />
+                    <img
+                      src={imgs.rightimg2}
+                      alt=""
+                      className="lowerimg h-[29.52px]"
+                    />
+                    <img src={imgs.rightimg3} alt="" className="lowerimg" />
+                    <img src={imgs.rightimg4} alt="" className="lowerimg" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-         </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
