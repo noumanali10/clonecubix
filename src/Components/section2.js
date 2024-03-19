2bddd25e144fd77866ba2276e24004c8663561d2
noumanali10 <alinaman1296@gmail.com>
2024-03-19 17:18:17 +0500

    update section2.js

--- src/Components/section2.js
+++ src/Components/section2.js
@@ -7,7 +7,7 @@ export default function section2() {
       <section className="  herolower flex justify-center items-center bg-gradient-to-r from-[#0bccee] to-[#ac1de1] h-auto text-white">
         <div className=" flex flex-col justify-around h-3/4 ">
           <div className="herolowertext Flex justify-around items-center h-full">
-            <div className="toptext text-xs font-normal text-[rgba(255,255,255,.5)] flex justify-center gap-5 w-full ">
+            <div className="mt-10 toptext text-xs font-normal text-[rgba(255,255,255,.5)] flex justify-center gap-5 w-full ">
               <span>DESIGN.</span>
               <span>DELVELOPMENT.</span>
               <span>CONSULTANCY.</span>
