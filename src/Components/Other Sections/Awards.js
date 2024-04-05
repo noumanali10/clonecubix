2c83e756f9a7323d00df04be51e4c74dc0eeda6e
noumanali10 <alinaman1296@gmail.com>
2024-04-05 10:44:35 +0500

    push

---
+++ src/Components/Other Sections/Awards.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// https://github.com/parthwebdev/react-carousels/blob/main/src/components/Carousel%2002/assets/arrow-left.svg?short_path=b2dd70a
+
+export default function Awards() {
+  return (
+    <></>
+  )
+}
